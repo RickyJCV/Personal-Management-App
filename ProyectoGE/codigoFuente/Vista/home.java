a26e5e0221cbed8f139129fb925f36390a373b0a
Ricky <Ricky@Ricky-PC>
2019-05-12 23:34:19 +0200

    Añadido Icono a botón de ver Turnos y Sueldo

--- ProyectoGE/codigoFuente/Vista/home.java
+++ ProyectoGE/codigoFuente/Vista/home.java
@@ -16,6 +16,8 @@ import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
+import javax.swing.ImageIcon;
+import javax.swing.LayoutStyle.ComponentPlacement;
 
 public class home extends JFrame {
 
@@ -34,6 +36,7 @@ public class home extends JFrame {
 		setContentPane(contentPane);
 		
 		JButton btnDarAltaUsuarios = new JButton("Dar Alta Usuarios");
+		btnDarAltaUsuarios.setBounds(40, 188, 219, 35);
 		btnDarAltaUsuarios.setFont(new Font("Consolas", Font.BOLD, 14));
 		btnDarAltaUsuarios.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -43,6 +46,7 @@ public class home extends JFrame {
 		});
 		
 		JButton btnVerUsuarios = new JButton("Ver Usuarios");
+		btnVerUsuarios.setBounds(40, 140, 219, 35);
 		btnVerUsuarios.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				tablaUsuarios verUsuarios = new tablaUsuarios();
@@ -52,6 +56,7 @@ public class home extends JFrame {
 		btnVerUsuarios.setFont(new Font("Consolas", Font.BOLD, 14));
 		
 		JButton btnAdministrarUsuarios = new JButton("Administrar Usuarios");
+		btnAdministrarUsuarios.setBounds(40, 234, 219, 37);
 		btnAdministrarUsuarios.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				administrarUsuarios verUsuarios = new administrarUsuarios();
@@ -61,6 +66,7 @@ public class home extends JFrame {
 		btnAdministrarUsuarios.setFont(new Font("Consolas", Font.BOLD, 14));
 		
 		JButton btnVerEmpleados = new JButton("Ver Empleados");
+		btnVerEmpleados.setBounds(309, 140, 219, 33);
 		btnVerEmpleados.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				tablaEmpleados verEmpleados = new tablaEmpleados();
@@ -70,6 +76,7 @@ public class home extends JFrame {
 		btnVerEmpleados.setFont(new Font("Consolas", Font.BOLD, 14));
 		
 		JButton btnAdministrarEmpleados = new JButton("Administrar Empleados");
+		btnAdministrarEmpleados.setBounds(309, 188, 219, 35);
 		btnAdministrarEmpleados.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				administrarEmpleados frame = new administrarEmpleados();
@@ -79,6 +86,8 @@ public class home extends JFrame {
 		btnAdministrarEmpleados.setFont(new Font("Consolas", Font.BOLD, 14));
 		
 		JButton btnVerTurnos = new JButton("VER TURNOS Y SUELDO");
+		btnVerTurnos.setBounds(77, 113, 421, 203);
+		btnVerTurnos.setIcon(new ImageIcon(home.class.getResource("/imagenes/verTurnos.png")));
 		btnVerTurnos.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				tablaEmpleados tabla = new tablaEmpleados();
@@ -88,99 +97,33 @@ public class home extends JFrame {
 		btnVerTurnos.setFont(new Font("Monospaced", Font.BOLD, 16));
 		
 		JLabel lblUser = new JLabel("\u00A1Bienvenido, para ver su turno y sueldo pulse el bot\u00F3n y \r\nintroduzca su C\u00F3digo!");
+		lblUser.setBounds(-82, 56, 746, 27);
 		lblUser.setHorizontalAlignment(SwingConstants.CENTER);
 		lblUser.setFont(new Font("Consolas", Font.BOLD, 12));
 		
 		JLabel lblAdmin = new JLabel("\u00A1Bienvenido al Sistema de Gesti\u00F3n de Empleados y Usuarios!");
+		lblAdmin.setBounds(-82, 29, 746, 21);
 		lblAdmin.setFont(new Font("Consolas", Font.BOLD, 16));
 		lblAdmin.setHorizontalAlignment(SwingConstants.CENTER);
 		
 		JLabel lblUsuariosAdmin = new JLabel("Gesti\u00F3n de Usuarios");
+		lblUsuariosAdmin.setBounds(77, 113, 276, 16);
 		lblUsuariosAdmin.setFont(new Font("Consolas", Font.BOLD, 15));
 		
 		JLabel lblEmpleadosAdmin = new JLabel("Gesti\u00F3n de Empleados");
+		lblEmpleadosAdmin.setBounds(352, 114, 279, 14);
 		lblEmpleadosAdmin.setFont(new Font("Consolas", Font.BOLD, 15));
-		GroupLayout gl_contentPane = new GroupLayout(contentPane);
-		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGap(5)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(10)
-							.addComponent(lblUser, GroupLayout.DEFAULT_SIZE, 557, Short.MAX_VALUE))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addComponent(lblAdmin, GroupLayout.DEFAULT_SIZE, 557, Short.MAX_VALUE)
-							.addGap(10))))
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGap(64)
-					.addComponent(lblUsuariosAdmin, GroupLayout.DEFAULT_SIZE, 178, Short.MAX_VALUE)
-					.addGap(99)
-					.addComponent(lblEmpleadosAdmin, GroupLayout.DEFAULT_SIZE, 183, Short.MAX_VALUE)
-					.addGap(43))
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGap(45)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addComponent(btnVerUsuarios, GroupLayout.DEFAULT_SIZE, 214, Short.MAX_VALUE)
-							.addGap(63)
-							.addComponent(btnAdministrarEmpleados, GroupLayout.DEFAULT_SIZE, 214, Short.MAX_VALUE))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addComponent(btnDarAltaUsuarios, GroupLayout.DEFAULT_SIZE, 214, Short.MAX_VALUE)
-							.addGap(277))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addComponent(btnAdministrarUsuarios, GroupLayout.DEFAULT_SIZE, 214, Short.MAX_VALUE)
-							.addGap(277))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(277)
-							.addComponent(btnVerEmpleados, GroupLayout.DEFAULT_SIZE, 214, Short.MAX_VALUE))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(115)
-							.addComponent(btnVerTurnos, GroupLayout.DEFAULT_SIZE, 237, Short.MAX_VALUE)
-							.addGap(139)))
-					.addGap(31))
-		);
-		gl_contentPane.setVerticalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGap(35)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(18)
-							.addComponent(lblUser, GroupLayout.DEFAULT_SIZE, 29, Short.MAX_VALUE))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addComponent(lblAdmin, GroupLayout.DEFAULT_SIZE, 23, Short.MAX_VALUE)
-							.addGap(24)))
-					.addGap(27)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addComponent(lblUsuariosAdmin, GroupLayout.PREFERRED_SIZE, 16, Short.MAX_VALUE)
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(1)
-							.addComponent(lblEmpleadosAdmin, GroupLayout.PREFERRED_SIZE, 14, Short.MAX_VALUE)
-							.addGap(1)))
-					.addGap(3)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(27)
-							.addComponent(btnVerUsuarios, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(73)
-							.addComponent(btnAdministrarEmpleados, GroupLayout.DEFAULT_SIZE, 35, Short.MAX_VALUE)
-							.addGap(48))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(73)
-							.addComponent(btnDarAltaUsuarios, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(119)
-							.addComponent(btnAdministrarUsuarios, GroupLayout.PREFERRED_SIZE, 37, GroupLayout.PREFERRED_SIZE))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(28)
-							.addComponent(btnVerEmpleados, GroupLayout.DEFAULT_SIZE, 33, Short.MAX_VALUE)
-							.addGap(95))
-						.addComponent(btnVerTurnos, GroupLayout.PREFERRED_SIZE, 140, GroupLayout.PREFERRED_SIZE))
-					.addGap(94))
-		);
-		contentPane.setLayout(gl_contentPane);
+		contentPane.setLayout(null);
+		contentPane.add(lblUser);
+		contentPane.add(lblAdmin);
+		contentPane.add(lblUsuariosAdmin);
+		contentPane.add(lblEmpleadosAdmin);
+		contentPane.add(btnVerUsuarios);
+		contentPane.add(btnAdministrarEmpleados);
+		contentPane.add(btnDarAltaUsuarios);
+		contentPane.add(btnAdministrarUsuarios);
+		contentPane.add(btnVerEmpleados);
+		contentPane.add(btnVerTurnos);
 
 		setLocationRelativeTo(null);
 		this.mod = mod;
